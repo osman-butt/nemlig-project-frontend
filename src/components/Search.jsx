53dbf7bec7036cad09f0365dc544403fb5e60556
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 00:12:05 +0100

    Hover effect on btns

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -7,13 +7,13 @@ export default function Search() {
         className="w-[535px] h-[53px] bg-white rounded-[6px] font-normal text-black text-[30px] tracking-[0] leading-[normal] whitespace-nowrap pl-[12px]"
       />
       <div>
-        <button className="w-[113px] h-[53px] top-0 left-px bg-[#d4793a] rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-8 mr-20 ">
+        <button className="w-[113px] h-[53px] top-0 left-px bg-[#d4793a] rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-8 mr-20  hover:bg-[#ecbc9a]">
           Søg
         </button>
-        <button className="w-[113px] h-[53px] top-0 left-px bg-white rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-10">
+        <button className="w-[113px] h-[53px] top-0 left-px bg-white rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-10  hover:bg-[#ecbc9a]">
           Sortér
         </button>
-        <button className="w-[113px] h-[53px] top-0 left-px bg-white rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-8">
+        <button className="w-[113px] h-[53px] top-0 left-px bg-white rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-8  hover:bg-[#ecbc9a]">
           Kategori
         </button>
       </div>
