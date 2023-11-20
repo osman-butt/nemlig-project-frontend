3ff876af25e3e8c4d2119dc44dc40361d393ab49
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 00:14:56 +0100

    Updated font

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,7 @@ export default function Card({ data }) {
             <div className="absolute w-[147px] top-[204px] left-[26px] opacity-60 [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[14px] text-center tracking-[0] leading-[normal]">
               {item.description}
             </div>
-            <button className="absolute w-[115px] h-[45px] top-[259px] left-[40px] all-[unset] box bg-[#d4793a] rounded-[4px] hover:bg-[#ecbc9a]  text-black font-bold ">
+            <button className="absolute w-[115px] h-[45px] top-[259px] left-[40px] all-[unset] box bg-[#d4793a] rounded-[4px] hover:bg-[#ecbc9a] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ">
               Læg i kurv
             </button>
             <img className="absolute w-[19px] h-[18px] top-[7px] left-[158px]" alt="Star" src={star} />
