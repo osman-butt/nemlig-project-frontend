52874cd41459363a18e70525e8dc155147292a46
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-20 12:34:01 +0100

    Rettet fra div til NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,48 @@
+import { NavLink } from "react-router-dom";
+
 export default function Navbar() {
   return (
-    <div className="w-[1920px] h-[54px]">
+    <nav className="w-[1920px] h-[54px]">
       <div className="fixed w-[1932px] h-[54px] top-0 left-0">
         <div className="relative w-[1920px] h-[54px] bg-[#58644c]">
-          <div className="w-[49px] left-[354px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+          <NavLink
+            to="/frost"
+            className="w-[49px] left-[354px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]"
+          >
             Frost
-          </div>
-          <div className="w-[49px] left-[652px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
-            Brød
-          </div>
-          <div className="w-[77px] left-[749px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
-            Kolonial
-          </div>
-          <div className="w-[94px] left-[874px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
-            Drikkevarer
-          </div>
-          <div className="w-[49px] left-[555px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
-            Kød
-          </div>
-          <div className="w-[56px] left-[451px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]">
+          </NavLink>
+          <NavLink
+            to="/mejeri"
+            className="w-[56px] left-[451px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]"
+          >
             Mejeri
-          </div>
+          </NavLink>
+          <NavLink
+            to="/kød"
+            className="w-[49px] left-[555px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]"
+          >
+            Kød
+          </NavLink>
+          <NavLink
+            to="/brød"
+            className="w-[49px] left-[652px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]"
+          >
+            Brød
+          </NavLink>
+          <NavLink
+            to="/kolonial"
+            className="w-[77px] left-[749px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]"
+          >
+            Kolonial
+          </NavLink>
+          <NavLink
+            to="/drikkevarer"
+            className="w-[94px] left-[874px] absolute h-[31px] top-[12px] [font-family:'Roboto-Regular',Helvetica] font-normal text-white text-[18px] tracking-[0] leading-[normal]"
+          >
+            Drikkevarer
+          </NavLink>
         </div>
       </div>
-    </div>
+    </nav>
   );
 }
--- src/pages/Landingpage.jsx
+++ src/pages/Landingpage.jsx
@@ -2,18 +2,15 @@ import image from "../assets/hero.jpg";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
 
 export default function Landingpage() {
   return (
-    <div
-      className="h-screen bg-fixed bg-center bg-cover"
-      style={{ backgroundImage: `url(${image})` }}
-    >
+    <div className="h-screen bg-fixed bg-center bg-cover" style={{ backgroundImage: `url(${image})` }}>
       <Header />
       <Hero />
+      <Navbar />
       <Footer />
     </div>
   );
 }
-
-
