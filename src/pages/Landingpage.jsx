e34c823deffcec51de0ebea8a73d0b1b103fb855
osman-butt <butt.osman@gmail.com>
2023-11-20 10:08:21 +0100

    Landingpage done

---
+++ src/pages/Landingpage.jsx
@@ -0,0 +1,19 @@
+import image from "../assets/hero.jpg";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+
+function Landingpage() {
+  return (
+    <div
+      className="h-screen bg-fixed bg-center bg-cover"
+      style={{ backgroundImage: `url(${image})` }}
+    >
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default Landingpage;
