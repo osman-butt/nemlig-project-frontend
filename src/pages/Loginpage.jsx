a710eb527d5855217f014528e47cb60610846358
gustavwiese <gusse12345@gmail.com>
2023-11-20 13:38:38 +0100

    Nav semi done. Need to show number of products in basket

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,10 +1,13 @@
 import image from "../assets/hero.jpg";
+import Nav from "../components/Nav";
 
 export default function Loginpage() {
   return (
     <div
       className="h-screen bg-fixed bg-center bg-cover"
-      style={{ backgroundImage: `url(${image})` }}
-    ></div>
+      style={{ backgroundImage: `url(${image})` }}>
+      <Nav />
+    </div>
+    
   );
 }
