3971a70d94e1da46b2da7307fb9580e3ce223d31
osman-butt <butt.osman@gmail.com>
2023-11-20 16:13:32 +0100

    basket route added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Landingpage from "./pages/Landingpage";
 import Shoppage from "./pages/Shoppage";
+import BasketPage from "./pages/BasketPage";
 import { Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Landingpage />} />
       <Route path="/shop" element={<Shoppage />} />
+      <Route path="/basket" element={<BasketPage />} />
     </Routes>
   );
 }
