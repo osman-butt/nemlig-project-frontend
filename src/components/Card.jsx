b087b604086861e0e3cba35b0a9027bdc9d632e8
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 00:17:02 +0100

    deleted some styling that did nothing

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,13 +11,13 @@ export default function Card({ data }) {
             <div className="absolute w-[147px] top-[180px] left-[26px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[18px] text-center tracking-[0] leading-[normal]">
               {item.product_name}
             </div>
-            <div className="absolute w-[78px] top-[228px] left-[61px] [font-family:'Roboto-Bold',Helvetica] font-bold text-black text-[18px] text-center tracking-[0] leading-[normal] whitespace-nowrap">
+            <div className="absolute w-[78px] top-[228px] left-[61px] [font-family:'Roboto-Bold',Helvetica] font-bold text-black text-[18px] text-center tracking-[0] leading-[normal]">
               {item.price}
             </div>
             <div className="absolute w-[147px] top-[204px] left-[26px] opacity-60 [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[14px] text-center tracking-[0] leading-[normal]">
               {item.description}
             </div>
-            <button className="absolute w-[115px] h-[45px] top-[259px] left-[40px] all-[unset] box bg-[#d4793a] rounded-[4px] hover:bg-[#ecbc9a] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ">
+            <button className="absolute w-[115px] h-[45px] top-[259px] left-[40px] bg-[#d4793a] rounded-[4px] hover:bg-[#ecbc9a] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ">
               Læg i kurv
             </button>
             <img className="absolute w-[19px] h-[18px] top-[7px] left-[158px]" alt="Star" src={star} />
