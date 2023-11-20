6c0c87fad14127014f3522bf76ed702bad7f5768
osman-butt <butt.osman@gmail.com>
2023-11-20 10:07:52 +0100

    Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  const companyName = "nemlig.com";
+  return (
+    <div className="w-full mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-white bg-[#58644C]">
+      <h1 className="font-bold text-center">
+        <Link to="/about">Om os</Link>
+      </h1>
+      <h3 className="mt-auto text-center">TEST</h3>
+      <div className="max-w-[1240px] m-auto">
+        <h1 className="w-full m-auto text-3xl font-bold text-[#d4793a]">
+          {companyName}
+        </h1>
+        <p className="pt-2">nemlig.com A/S</p>
+        <p>Banemarksvej 58</p>
+        <p>2605 Brøndby</p>
+        <p>CVR 33070861</p>
+        <p className="font-extrabold text-[#d4793a] pt-2">70 33 72 33</p>
+        <p className="font-extrabold text-[#d4793a]">kontakt@nemlig.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
