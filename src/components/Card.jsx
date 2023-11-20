be67958bea191cbd0a993c1e362152b559c9a2c0
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 00:09:40 +0100

    Hover effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,13 +17,8 @@ export default function Card({ data }) {
             <div className="absolute w-[147px] top-[204px] left-[26px] opacity-60 [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[14px] text-center tracking-[0] leading-[normal]">
               {item.description}
             </div>
-            <button className="absolute w-[115px] h-[45px] top-[259px] left-[40px] all-[unset] box-border">
-              <div className="relative w-[113px] h-[45px]">
-                <div className="absolute w-[107px] h-[45px] top-0 left-[3px] bg-[#d4793a] rounded-[4px]" />
-                <div className="absolute w-[113px] top-[9px] left-0 [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[18px] text-center tracking-[0] leading-[normal]">
-                  Læg i kurv
-                </div>
-              </div>
+            <button className="absolute w-[115px] h-[45px] top-[259px] left-[40px] all-[unset] box bg-[#d4793a] rounded-[4px] hover:bg-[#ecbc9a]  text-black font-bold ">
+              Læg i kurv
             </button>
             <img className="absolute w-[19px] h-[18px] top-[7px] left-[158px]" alt="Star" src={star} />
           </div>
