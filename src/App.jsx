c2d8e3d96af628a4400eeea8ff38f681f84bd76c
Kapalee <Shamvictor@hotmail.com>
2023-11-21 09:16:38 +0100

    Orderpage
    Lavet orderpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Landingpage from "./pages/Landingpage";
 import Shoppage from "./pages/Shoppage";
+import Orderpage from "./pages/Orderpage";
 import { Route, Routes } from "react-router-dom";
 
 export default function App() {
@@ -7,6 +8,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Landingpage />} />
       <Route path="/shop" element={<Shoppage />} />
+      <Route path="/order" element={<Orderpage />} />
     </Routes>
   );
 }
