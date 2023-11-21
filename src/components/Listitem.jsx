e14405ab4ee0d70327c86b24a7920226dbaf85a5
osman-butt <butt.osman@gmail.com>
2023-11-21 12:34:33 +0100

    Adding dummy data
    Added:
    - Dummy data for display
    - Usestate
    - onclick events for toggling quantity

--- src/components/Listitem.jsx
+++ src/components/Listitem.jsx
@@ -1,6 +1,6 @@
 import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
 
-function Listitem() {
+function Listitem({ product, addQuantity, deductQuantity }) {
   return (
     <>
       <tr className="text-left text-[14px] border-gray-500 border-b">
@@ -8,34 +8,46 @@ function Listitem() {
           <div className="flex items-center py-2 align-middle">
             <img
               className="w-[30px] "
-              src="https://d2dql7oeescq6w.cloudfront.net/100004/1-small-OJq52ElK9p.webp"
+              src={product.images[0].image_link}
               alt="product-img"
             />
           </div>
         </td>
         <td className="w-[40%] py-4" data-field="product-name">
           <p className="w-[135px] font-bold truncate md:hidden">
-            JORDBÆRMARMELADE
+            {product.product_name}
           </p>
-          <p className="hidden font-bold md:block">JORDBÆRMARMELADE </p>
-          <p className="font-light opacity-70">285 GR. / EASIS</p>
+          <p className="hidden font-bold md:block">{product.product_name}</p>
+          <p className="font-light">{product.product_underline}</p>
         </td>
         <td className="w-[20%]" data-field="price">
-          <p className="line-through">12.50 kr.</p>
-          <p className="font-bold">10.00 kr.</p>
+          {product.prices.length > 1 ? (
+            <>
+              <p className="line-through">
+                {product.prices[0].price.toFixed(2)} kr.
+              </p>
+              <p className="font-bold">
+                {product.prices[1].price.toFixed(2)} kr.
+              </p>
+            </>
+          ) : (
+            <p className="font-bold">
+              {product.prices[0].price.toFixed(2)} kr.
+            </p>
+          )}
         </td>
         <td className="w-[10%] text-center" data-field="toggle-quantity">
           <div className="flex flex-row items-center justify-center align-middle">
             <CiCircleMinus
               size={30}
-              onClick={() => console.log("Minus clicked")}
+              onClick={() => deductQuantity(product.product_id)}
             />
             <p className="mx-1 w-[30px] py-1 px-1 bg-[#58644c] rounded-full font-bold text-white">
-              {1}
+              {product.quantity}
             </p>
             <CiCirclePlus
               size={30}
-              onClick={() => console.log("Plus clicked")}
+              onClick={() => addQuantity(product.product_id)}
             />
           </div>
         </td>
