e8437f3117bffd0d970cead7bb1d3ebe3c6d6dd8
Kapalee <Shamvictor@hotmail.com>
2023-11-21 19:58:37 +0100

    responsive work

--- src/components/Confirm.jsx
+++ src/components/Confirm.jsx
@@ -2,15 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function Confirm() {
   return (
-    <div className="flex items-center justify-center py-20">
-      <div className="relative w-[660px] h-[466px]">
-        <div className="relative w-[664px] h-[466px] top-0 left-0">
+    <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+      <div className="relative">
+        <div className="relative">
           <div className="relative w-[660px] h-[466px] top-0 left-0 bg-[#e8e3d8] rounded-[8px]">
-            <div className="absolute w-[352px] h-[75px] top-[30px] left-[57px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[36px] tracking-[0] leading-[normal]">
+            <div className="absolute w-[352px] h-[75px] top-[30px] left-[57px] [font-general] font-normal text-black text-[36px] tracking-[0] leading-[normal]">
               Ordrebekræftelse
             </div>
             <div className="absolute w-[532px] h-[266px] top-[157px] left-[53px]">
-              <p className="absolute w-[516px] top-0 left-[12px] [font-family:'Roboto-Bold',Helvetica] font-bold text-black text-[24px] tracking-[0] leading-[normal]">
+              <p className="absolute w-[516px] top-0 left-[12px] [font-general] font-bold text-black md:text-2xl sm:text-1xl md:py-1 tracking-[0] leading-[normal]">
                 Tak for din ordre!
                 <br />
                 <br />
@@ -18,7 +18,7 @@ export default function Confirm() {
               </p>
               <Link to="/">
                 <button
-                  className="absolute w-[532px] h-[66px] top-[200px] left-0 [font-family:'Roboto-Bold',Helvetica] font-bold text-black text-[24px] tracking-[0] leading-[normal]"
+                  className="absolute w-[532px] h-[66px] top-[200px] left-0 [font-general] font-bold text-white text-[24px] tracking-[0] leading-[normal] border border-black rounded-md px-4 bg-[#58644c]"
                   alt="Bestil btn"
                   src="bestil-btn.png"
                 >
@@ -28,11 +28,6 @@ export default function Confirm() {
               </Link>
             </div>
           </div>
-          <img
-            className="absolute w-[522px] h-px top-[-2700px] left-[-5457px]"
-            alt="Line"
-            src="line-3.svg"
-          />
         </div>
       </div>
     </div>
