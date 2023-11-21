b9e3eec17fcc63cdafe5aab8ca2a503a7332dd95
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 14:44:21 +0100

    Padding inside the input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,19 +5,15 @@ export default function Search() {
         <input
           type="text"
           placeholder="Søg efter varer..."
-          className="lg:w-[535px] h-[53px] bg-white rounded font-normal text-black text-[20px]"
+          className="lg:w-[535px] h-[53px] bg-white rounded font-normal text-black text-[20px] pl-4"
         />
         <button className="h-[53px] px-4 bg-[#d4793a] rounded font-normal text-black lg:text-[20px] md:text-[20px] sm:text-[15px] hover:bg-[#ecbc9a]">
           Søg
         </button>
       </form>
       <div className="hidden gap-4 md:flex">
-        <button className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">
-          Sortér
-        </button>
-        <button className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">
-          Kategori
-        </button>
+        <button className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">Sortér</button>
+        <button className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">Kategori</button>
       </div>
     </div>
   );
