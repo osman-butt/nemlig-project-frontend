037f5c9f30c3dadd64e6f38fd906859a31490acf
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 15:52:12 +0100

    Added product underline

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,14 +14,16 @@ export default function Card({ data }) {
     <div className="flex flex-wrap justify-center">
       {data[0].map((item) => (
         <div key={item.product_id} className="bg-white m-4 border border-gray-200 rounded">
+          <div>
           <div className="relative w-[200px] h-[317px]">
             <div>
-              <img className="w-[19px] h-[18px] ml-auto mt-4 mr-4" alt="Star" src={star} />
+              <img className="w-[19px] h-[18px] ml-auto mt-2 mr-4" alt="Star" src={star} />
             </div>
             <div className="w-full h-[150px] m-auto mt-5">
               <img className="w-[80px] h-[140px] object-cover m-auto" alt="Image" src={item.images[0].image_link} />
             </div>
-            <div className="relative w-full m-auto mt-2 font-medium text-black text-[16px] text-center">{item.product_name}</div>
+            <div className="relative w-full m-auto font-medium text-black text-[16px] text-center">{item.product_name}</div>
+            <div className="w-full m-auto font-normal text-[12px] opacity-80 text-center">{item.product_underline}</div>
             <div className="w-full m-auto font-bold text-black text-[18px] text-center">{item.prices[0].price} kr.</div>
             <button
               onClick={addToBasket}
@@ -30,6 +32,7 @@ export default function Card({ data }) {
               Læg i kurv
             </button>
           </div>
+        </div>
         </div>
       ))}
     </div>
