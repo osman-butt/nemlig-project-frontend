b8bb22688566e21d925e0604b2e3f8571970ad32
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-21 13:08:34 +0100

    Made search responsive (sm view not done)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,20 @@
 export default function Search() {
   return (
-    <div className="relative top-0 left-0 mt-4 ml-40 flex justify-center">
+    <div className="lg:mt-4 sm:mt-0 flex flex-row justify-center">
       <input
         type="text"
         placeholder="Søg efter varer..."
-        className="w-[535px] h-[53px] bg-white rounded-[6px] font-normal text-black text-[30px] tracking-[0] leading-[normal] whitespace-nowrap pl-[12px]"
+        className="w-full lg:w-[535px] md:w-[400px] h-[53px] bg-white rounded-[6px] font-normal text-black text-[20px] lg:text-[20px]  [font-family:'Roboto-Regular',Helvetica] pl-[12px] lg:mb-0 lg:mr-2"
       />
-      <div>
-        <button className="w-[113px] h-[53px] top-0 left-px bg-[#d4793a] rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-8 mr-20  hover:bg-[#ecbc9a]">
-          Søg
-        </button>
-        <button className="w-[113px] h-[53px] top-0 left-px bg-white rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-10  hover:bg-[#ecbc9a]">
-          Sortér
-        </button>
-        <button className="w-[113px] h-[53px] top-0 left-px bg-white rounded-[4px] [font-family:'Roboto-Regular',Helvetica] font-normal text-black text-[20px] text-center tracking-[0] leading-[normal] ml-8  hover:bg-[#ecbc9a]">
-          Kategori
-        </button>
-      </div>
+      <button className="lg:w-[113px] h-[53px] bg-[#d4793a] rounded-[4px] font-normal text-black text-[15px] lg:text-[20px] [font-family:'Roboto-Regular',Helvetica] lg:mb-0 lg:mr-12 lg:ml-2 hover:bg-[#ecbc9a]">
+        Søg
+      </button>
+      <button className="lg:w-[113px] h-[53px] bg-white rounded-[4px] font-normal text-black text-[15px] lg:text-[20px]  [font-family:'Roboto-Regular',Helvetica] lg:mb-0 lg:mr-6 lg:ml-20 hover:bg-[#ecbc9a]">
+        Sortér
+      </button>
+      <button className="lg:w-[113px] h-[53px] bg-white rounded-[4px] font-normal text-black text-[15px] lg:text-[20px]  [font-family:'Roboto-Regular',Helvetica] lg:mb-0 lg:mr-4 hover:bg-[#ecbc9a]">
+        Kategori
+      </button>
     </div>
   );
 }
