6a2f48357add0abf70765d48b953e077927671aa
osman-butt <butt.osman@gmail.com>
2023-11-22 09:58:31 +0100

    Fixing navbar content issue

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -9,7 +9,7 @@ export default function Items() {
       try {
         const response = await fetch("./src/test-data.json");
         const data = await response.json();
-        const dataArray = Object.values(data);
+        const dataArray = Object.values(data)[0];
         setData(dataArray);
       } catch (err) {
         console.log(err);
@@ -18,6 +18,13 @@ export default function Items() {
     fetchData();
   }, []);
 
-  return <Card data={data} />;
-
+  return (
+    <div className="max-w-[1240px] mx-auto">
+      <div className="grid justify-center grid-cols-1 gap-4 p-4 mt-4 rounded sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1100px]:grid-cols-5">
+        {data.map(item => (
+          <Card key={item.product_id} data={item} />
+        ))}
+      </div>
+    </div>
+  );
 }
