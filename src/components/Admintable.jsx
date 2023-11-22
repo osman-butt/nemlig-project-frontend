86f105bce82c4c4053be7494884f3a2d379efe07
gustavwiese <gusse12345@gmail.com>
2023-11-22 17:52:17 +0100

    Added the first createdialog and beginning of updatedialog
    Updatedialog messing with me, i click on the update but the create dialog opens up

--- src/components/Admintable.jsx
+++ src/components/Admintable.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import { AiOutlineEdit, AiOutlineClose } from "react-icons/ai";
-
+import Createdialog from "./Createdialog";
+import Updatedialog from "./Updatedialog";
 
 export default function Admintable() {
+    const [dialogOpen, setDialogOpen] = useState(false);
+
     const editIcon = <AiOutlineEdit size={30}/>
     const deleteIcon = <AiOutlineClose size={30}/>
+
+    const openDialog = () => {
+        setDialogOpen(true);
+    }
+
+    const closeDialog = () => {
+        setDialogOpen(false);
+    }
    
   return (
     <div className="font-general">
         <div className="bg-[#e8e3d8] bg-opacity- my-5 py-2 max-w-[1240px] mx-auto">
-    <button className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md my-4 ml-3 py-2 text-black font-medium text-xl">
-        Opret
-    </button>
     
+    <button onClick={openDialog} className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md my-4 ml-3 py-2 text-black font-medium text-xl">
+        Tilføj
+    </button>
+    {dialogOpen && (
+          <Createdialog closeDialog={closeDialog}>
+          </Createdialog>
+        )}
 
     <div className="mx-3">
     <table className="border-black border-solid border-2 w-full font-[700]">
@@ -28,7 +44,8 @@ export default function Admintable() {
     <td className="border-black border-solid border-2">1</td>
     <td className="border-black border-solid border-2">Kanel</td>
     <td className="border-black border-solid border-2">10</td>
-    <td className="border-black border-solid border-2 hover:text-[#d4793a] cursor-pointer text-center align-middle">{editIcon}</td>
+    <td onClick={openDialog} className="border-black border-solid border-2 hover:text-[#d4793a] cursor-pointer text-center align-middle">{editIcon}</td>
+   
     <td className="border-black border-solid border-2 hover:text-[#d4793a] cursor-pointer">{deleteIcon}</td>
   </tr>
 
