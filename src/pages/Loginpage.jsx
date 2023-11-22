1fe1d3aca95723c532bb3eb75a6923b0f2cb4e84
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-22 14:20:41 +0100

    Added className prop to inputfield, and made some responsive styling for loginform

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,9 +1,11 @@
 import image from "../assets/hero.jpg";
 import Header from "../components/Header";
 import Loginform from "../components/Loginform";
+import Footer from "../components/Footer";
 
 export default function Loginpage() {
   return (
+    <>
     <div
       className="h-screen bg-fixed bg-center bg-cover"
       style={{ backgroundImage: `url(${image})` }}
@@ -11,5 +13,7 @@ export default function Loginpage() {
       <Header />
       <Loginform />
     </div>
+    <Footer />
+    </>
   );
 }
