c79659316b2299bb07691f72660b56b548061c45
osman-butt <butt.osman@gmail.com>
2023-11-23 21:49:20 +0100

    Basket state
    - Adding basket state logic to app.jsx
    - Header pulled out of components and into app.jsx (instead) of passing props to each page

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,86 @@ import { Route, Routes } from "react-router-dom";
 import Loginpage from "./pages/Loginpage";
 import Createaccountpage from "./pages/Createaccountpage.jsx";
 import Adminpage from "./pages/Adminpage.jsx";
+import { useEffect, useState } from "react";
+import Header from "./components/Header.jsx";
 // import Formdialog from "./components/Formdialog.jsx";
 
 export default function App() {
+  const [basket, setBasket] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./src/basket.json");
+        const data = await response.json();
+        const dataArray = Object.values(data)[0];
+        setBasket(dataArray);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // Function to add quantity
+  const addQuantity = product_id => {
+    const productIndex = basket.findIndex(
+      item => item.product_id === product_id
+    );
+
+    const updatedBasket = [...basket];
+    updatedBasket[productIndex] = {
+      ...updatedBasket[productIndex],
+      quantity: updatedBasket[productIndex].quantity + 1,
+    };
+
+    setBasket(updatedBasket);
+  };
+
+  // Function to deduct quantity
+  const deductQuantity = product_id => {
+    const productIndex = basket.findIndex(
+      item => item.product_id === product_id
+    );
+
+    const updatedBasket = [...basket];
+    updatedBasket[productIndex] = {
+      ...updatedBasket[productIndex],
+      quantity: updatedBasket[productIndex].quantity - 1,
+    };
+
+    updatedBasket[productIndex].quantity === 0 &&
+      updatedBasket.splice(productIndex, 1);
+
+    setBasket(updatedBasket);
+  };
+
   return (
-    <Routes>
-      <Route path="/" element={<Landingpage />} />
-      <Route path="/login" element={<Loginpage />} />
-      <Route path="/createaccount" element={<Createaccountpage />} />
-      <Route path="/shop" element={<Shoppage />} />
-      <Route path="/order" element={<Orderpage />} />
-      <Route path="/basket" element={<BasketPage />} />
-      <Route path="/admin" element={<Adminpage />} />
-      {/* <Route path="/formdialog" element={<Formdialog />} /> */}
-    </Routes>
+    <>
+      <Header
+        basket={basket}
+        addQuantity={addQuantity}
+        deductQuantity={deductQuantity}
+      />
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/login" element={<Loginpage />} />
+        <Route path="/createaccount" element={<Createaccountpage />} />
+        <Route path="/shop" element={<Shoppage />} />
+        <Route path="/order" element={<Orderpage />} />
+        <Route
+          path="/basket"
+          element={
+            <BasketPage
+              basket={basket}
+              addQuantity={addQuantity}
+              deductQuantity={deductQuantity}
+            />
+          }
+        />
+        <Route path="/admin" element={<Adminpage />} />
+        {/* <Route path="/formdialog" element={<Formdialog />} /> */}
+      </Routes>
+    </>
   );
 }
