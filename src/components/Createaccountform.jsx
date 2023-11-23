113ee2e14689dd75753a8e8f0bb0c439d3d052ab
osman-butt <butt.osman@gmail.com>
2023-11-23 21:52:35 +0100

    General top padding

--- src/components/Createaccountform.jsx
+++ src/components/Createaccountform.jsx
@@ -5,21 +5,42 @@ export default function Createaccountform() {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full max-w-lg flex flex-col m-auto justify-center text-black lg:mt-10 p-4">
+    <div className="flex flex-col justify-center w-full max-w-lg py-8 m-auto text-black">
       <div className="bg-[#e8e3d8] rounded">
-        <h2 className="font-medium text-2xl py-3 md:text-2xl text-center">Opret bruger</h2>
+        <h2 className="py-3 text-2xl font-medium text-center md:text-2xl">
+          Opret bruger
+        </h2>
 
-        <form className="w-full grid justify-around gap-y-2">
-          <InputField label="Navn:" type="text" placeholder="Skriv dit fulde navn her" />
-          <InputField label="Adresse:" type="text" placeholder="Skriv din adresse her" />
-          <InputField label="Postnummer:" type="text" placeholder="Postnummer" />
+        <form className="grid justify-around w-full gap-y-2">
+          <InputField
+            label="Navn:"
+            type="text"
+            placeholder="Skriv dit fulde navn her"
+          />
+          <InputField
+            label="Adresse:"
+            type="text"
+            placeholder="Skriv din adresse her"
+          />
+          <InputField
+            label="Postnummer:"
+            type="text"
+            placeholder="Postnummer"
+          />
           <InputField label="By:" type="text" placeholder="By" />
           <InputField label="E-mail:" type="email" placeholder="E-mail" />
-          <InputField label="Password:" type="password" placeholder="Password" />
+          <InputField
+            label="Password:"
+            type="password"
+            placeholder="Password"
+          />
         </form>
 
         <div className="flex items-center justify-between px-3 m-6">
-          <button onClick={() => navigate("/createaccount")} className="bg-[#58644C] hover:bg-[#ecbc9a] w-full rounded-md py-3 font-medium">
+          <button
+            onClick={() => navigate("/createaccount")}
+            className="bg-[#58644C] hover:bg-[#ecbc9a] w-full rounded-md py-3 font-medium"
+          >
             Opret bruger
           </button>
         </div>
--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -6,17 +6,23 @@ export default function Loginform() {
 
   //<div className="flex flex-col items-center justify-center max-w-[1240px] text-black">
   return (
-    <div className="w-full max-w-lg m-auto mt-20 p-4">
+    <div className="w-full max-w-lg pt-8 m-auto">
       <div className="bg-[#e8e3d8] rounded">
-        <h2 className="font-medium text-2xl py-3 text-center">Log ind</h2>
+        <h2 className="py-3 text-2xl font-medium text-center">Log ind</h2>
 
-        <form className="w-full grid justify-around gap-y-2">
+        <form className="grid justify-around w-full gap-y-2">
           <InputField label="Email:" type="email" placeholder="Email" />
-          <InputField label="Password:" type="password" placeholder="Password" />
+          <InputField
+            label="Password:"
+            type="password"
+            placeholder="Password"
+          />
         </form>
 
         <div className="flex items-center justify-between px-4">
-          <button className="bg-[#d4793a] hover:bg-[#ecbc9a] w-[400px] rounded-md my-4 mx-auto py-3 text-black font-medium">Log ind</button>
+          <button className="bg-[#d4793a] hover:bg-[#ecbc9a] w-[400px] rounded-md my-4 mx-auto py-3 text-black font-medium">
+            Log ind
+          </button>
         </div>
         <div className="flex items-center justify-between px-4">
           <button
@@ -26,7 +32,9 @@ export default function Loginform() {
             Opret bruger
           </button>
         </div>
-        <p className="py-4 font-medium underline cursor-pointer text-center">Glemt password?</p>
+        <p className="py-4 font-medium text-center underline cursor-pointer">
+          Glemt password?
+        </p>
       </div>
     </div>
   );
