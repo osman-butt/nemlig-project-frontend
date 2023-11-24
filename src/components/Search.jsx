141f14e604c03f4505b98e1d1af41888de6d8a2f
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-24 11:17:15 +0100

    Changed btn to select instead.

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,5 @@
-export default function Search({onSort, }) {
+export default function Search({onSort }) {
 
-  function handleSortClick(){
-    onSort("asc")
-  }
 
   return (
     <div className="max-w-[1240px] flex flex-row justify-center m-auto mt-4 font-general align-middle gap-10">
@@ -17,7 +14,11 @@ export default function Search({onSort, }) {
         </button>
       </form>
       <div className="hidden gap-4 md:flex">
-        <button onClick={handleSortClick} className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">Sortér</button>
+        <select onChange={(e) => onSort(e.target.value)} className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">
+          <option value="">Sorter efter</option>
+          <option value="asc">A-Å</option>
+          <option value="desc">Å-A</option>
+        </select>
         <button className="h-[53px] px-4 bg-white rounded font-normal text-[20px] text-black  hover:bg-[#ecbc9a]">Kategori</button>
       </div>
     </div>
