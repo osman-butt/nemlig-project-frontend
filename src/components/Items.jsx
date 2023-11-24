887b0678c16b3fc584a3d79c5ebfb9c1cf04cdcb
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-24 11:35:16 +0100

    fixed prop names to be similar

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,15 +1,10 @@
-import { useState } from "react";
 import Card from "./Card";
 
-export default function Items({ data }) {
-
-
-
-
+export default function Items({ products }) {
   return (
     <div className="max-w-[1240px] mx-auto">
       <div className="grid justify-center grid-cols-1 gap-4 p-4 mt-4 rounded sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 min-[1100px]:grid-cols-5">
-        {data.map(item => (
+        {products.map(item => (
           <Card key={item.product_id} data={item} />
         ))}
       </div>
--- src/pages/Shoppage.jsx
+++ src/pages/Shoppage.jsx
@@ -1,54 +1,45 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import image from "../assets/hero.jpg";
-import Header from "../components/Header";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
 import Items from "../components/Items";
 
 export default function Shoppage() {
-const [item, setItem] = useState([]);
-const [sort, setSort] = useState("");
+  const [products, setProducts] = useState([]);
+  const [sort, setSort] = useState("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("./src/products-data.json");
+        const data = await response.json();
+        const dataArray = Object.values(data)[0];
 
+        if (sort === "asc") {
+          dataArray.sort((a, b) => a.product_name.localeCompare(b.product_name));
+        } else if (sort === "desc") {
+          dataArray.sort((a, b) => b.product_name.localeCompare(a.product_name));
+        }
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await fetch("./src/products-data.json");
-      const data = await response.json();
-      const dataArray = Object.values(data)[0];
-
-      if (sort === "asc"){
-        dataArray.sort((a, b) => a.product_name.localeCompare(b.product_name));
-      } else if (sort === "desc") {
-        dataArray.sort((a, b) => b.product_name.localeCompare(a.product_name));
+        setProducts(dataArray);
+      } catch (err) {
+        console.log(err);
       }
+    };
+    fetchData();
+  }, [sort]);
 
-      setItem(dataArray);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-  fetchData();
-}, [sort]);
-  
-
-function handleSort(sortOptions){
-  setSort(sortOptions);
-}
-
+  function handleSort(sortOptions) {
+    setSort(sortOptions);
+  }
 
   return (
     <>
-      <div
-        className="min-h-screen bg-fixed bg-center bg-cover"
-        style={{ backgroundImage: `url(${image})` }}
-      >
-
+      <div className="min-h-screen bg-fixed bg-center bg-cover" style={{ backgroundImage: `url(${image})` }}>
         <Navbar />
-        <Search onSort={handleSort}  />
-        <Items data={item} />
+        <Search onSort={handleSort} />
+        <Items products={products} />
       </div>
       <Footer />
     </>
