85703721ffcd8aaedce8e8765aba68e645e6993e
Benjamin-Harris1 <beha1503@gmail.com>
2023-11-29 22:16:26 +0100

    Fetch from backend and set up checks so it doesn't crash if the data prop is not present in card

--- src/pages/Shoppage.jsx
+++ src/pages/Shoppage.jsx
@@ -13,7 +13,7 @@ export default function Shoppage({ addToBasket }) {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("./src/products.json");
+        const response = await fetch("http://localhost:3000/products");
         const data = await response.json();
         let dataArray = Object.values(data)[0];
 
