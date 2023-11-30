bb3f0032f69516e68eff1e80a07925dafa86f42c
gustavwiese <gusse12345@gmail.com>
2023-11-30 16:08:07 +0100

    admintable prettier and with image, what do u think?

--- src/components/Admintable.jsx
+++ src/components/Admintable.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useState, useEffect } from "react";
 import Table from "./Table";
 import Createdialog from "./Createdialog";
+
 
 export default function Items() {
   const [dialogOpen, setDialogOpen] = useState(false);
@@ -11,7 +11,7 @@ export default function Items() {
   const closeDialog = () => {
     setDialogOpen(false);
   };
-
+  
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -29,19 +29,13 @@ export default function Items() {
   }, []);
 
   return (
-    <div className="mb-20 mx-24">
+    <div className="mx-5 py-5">
       {dialogOpen && <Createdialog closeDialog={closeDialog} />}
-
-      <button
-        onClick={openDialog}
-        className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md my-4 ml-3 py-2 text-black font-medium text-xl"
-      >
+       <button onClick={openDialog} className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md my-4 py-2 text-black font-medium text-xl">
         Tilføj
       </button>
       {data.map((item) => (
-        <React.Fragment key={item.product_id}>
-          <Table data={item} />
-        </React.Fragment>
+        <Table key={item.product_id} data={item} />
       ))}
     </div>
   );
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -28,28 +28,31 @@ export default function Table({ data }) {
         <Deletedialog closeDialog={closeDialog} />
       )}
 
-      <table className="border-black border-solid border-2 w-full font-[700] bg-[#e8e3d8]">
+      <table className="font-medium bg-[#e8e3d8]">
         <tbody>
-          <tr className="border-black border-solid border-2 text-center">
-            <td className="border-black border-solid border-2 p-2 w-1/5">
+          <tr className="text-center text-xs sm:text-lg md:text-xl">
+            <td className="border-black border-solid border-2 p-2 w-[8%]">
               {data.product_id}
+            </td>
+            <td className="border-black border-solid border-2 p-2 w-[8%]">
+              <img className="w-[50%] mx-auto" src={data.images[0].image_link} alt="" />
             </td>
             <td className="border-black border-solid border-2 p-2 w-2/5">
               {data.product_name}
             </td>
             <td className="border-black border-solid border-2 p-2 w-1/5">
-              {data.prices[0].price}
+              {data.prices[0].price + " kr."}
             </td>
             <td
               onClick={() => openDialog("update")}
-              className="border-black border-solid border-2 p-2 w-1/5 hover:text-[#d4793a] cursor-pointer text-center align-middle"
+              className="border-black border-solid border-2 p-2 w-[2%] hover:text-[#d4793a] cursor-pointer text-center align-middle"
             >
               {editIcon}
             </td>
 
             <td
               onClick={() => openDialog("delete")}
-              className="border-black border-solid border-2 p-2 w-1/5 hover:text-[#d4793a] cursor-pointer"
+              className="border-black border-solid border-2 p-2 w-[2%] hover:text-[#d4793a] cursor-pointer"
             >
               {deleteIcon}
             </td>
