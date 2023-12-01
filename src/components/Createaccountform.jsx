5281b7af26cd9f9ea0ef07084cd661af2928b7e4
Benjamin-Harris1 <beha1503@gmail.com>
2023-12-01 15:28:02 +0100

    Added padding to fit responsive design

--- src/components/Createaccountform.jsx
+++ src/components/Createaccountform.jsx
@@ -5,42 +5,21 @@ export default function Createaccountform() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col justify-center w-full max-w-lg py-8 m-auto text-black">
+    <div className="flex flex-col justify-center w-full max-w-lg py-8 m-auto text-black px-4">
       <div className="bg-[#e8e3d8] rounded">
-        <h2 className="py-3 text-2xl font-medium text-center md:text-2xl">
-          Opret bruger
-        </h2>
+        <h2 className="py-3 text-2xl font-medium text-center md:text-2xl">Opret bruger</h2>
 
         <form className="grid justify-around w-full gap-y-2">
-          <InputField
-            label="Navn:"
-            type="text"
-            placeholder="Skriv dit fulde navn her"
-          />
-          <InputField
-            label="Adresse:"
-            type="text"
-            placeholder="Skriv din adresse her"
-          />
-          <InputField
-            label="Postnummer:"
-            type="text"
-            placeholder="Postnummer"
-          />
+          <InputField label="Navn:" type="text" placeholder="Skriv dit fulde navn her" />
+          <InputField label="Adresse:" type="text" placeholder="Skriv din adresse her" />
+          <InputField label="Postnummer:" type="text" placeholder="Postnummer" />
           <InputField label="By:" type="text" placeholder="By" />
           <InputField label="E-mail:" type="email" placeholder="E-mail" />
-          <InputField
-            label="Password:"
-            type="password"
-            placeholder="Password"
-          />
+          <InputField label="Password:" type="password" placeholder="Password" />
         </form>
 
         <div className="flex items-center justify-between px-3 m-6">
-          <button
-            onClick={() => navigate("/createaccount")}
-            className="bg-[#58644C] hover:bg-[#ecbc9a] w-full rounded-md py-3 font-medium"
-          >
+          <button onClick={() => navigate("/createaccount")} className="bg-[#58644C] hover:bg-[#ecbc9a] w-full rounded-md py-3 font-medium">
             Opret bruger
           </button>
         </div>
--- src/components/Loginform.jsx
+++ src/components/Loginform.jsx
@@ -6,23 +6,17 @@ export default function Loginform() {
 
   //<div className="flex flex-col items-center justify-center max-w-[1240px] text-black">
   return (
-    <div className="w-full max-w-lg pt-8 m-auto">
+    <div className="w-full max-w-lg pt-8 m-auto px-4">
       <div className="bg-[#e8e3d8] rounded">
         <h2 className="py-3 text-2xl font-medium text-center">Log ind</h2>
 
         <form className="grid justify-around w-full gap-y-2">
           <InputField label="Email:" type="email" placeholder="Email" />
-          <InputField
-            label="Password:"
-            type="password"
-            placeholder="Password"
-          />
+          <InputField label="Password:" type="password" placeholder="Password" />
         </form>
 
         <div className="flex items-center justify-between px-4">
-          <button className="bg-[#d4793a] hover:bg-[#ecbc9a] w-[400px] rounded-md my-4 mx-auto py-3 text-black font-medium">
-            Log ind
-          </button>
+          <button className="bg-[#d4793a] hover:bg-[#ecbc9a] w-[400px] rounded-md my-4 mx-auto py-3 text-black font-medium">Log ind</button>
         </div>
         <div className="flex items-center justify-between px-4">
           <button
@@ -32,9 +26,7 @@ export default function Loginform() {
             Opret bruger
           </button>
         </div>
-        <p className="py-4 font-medium text-center underline cursor-pointer">
-          Glemt password?
-        </p>
+        <p className="py-4 font-medium text-center underline cursor-pointer">Glemt password?</p>
       </div>
     </div>
   );
