44c995ea6c3fd398a38412f4f55f4e6cf59643df
Benjamin-Harris1 <beha1503@gmail.com>
2023-12-04 17:04:14 +0100

    Same styling for navbar buttons + changed pagination to danish

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,50 @@
 export default function Navbar({ setCategory, setPage }) {
+
+  const buttonStyle = "text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer"
+
   return (
     <nav>
       <div className="bg-[#58644c] w-full font-general">
         <div className="max-w-[1240px] flex flex-wrap justify-center md:justify-start m-auto z-50 py-2 align-middle">
           <button
           onClick={() => setCategory("")}
-          className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer"
+          className={buttonStyle}
           >
             Alle
           </button>
           <button
             onClick={() => {setCategory("frost"); setPage(1)}}
-            className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer"
+            className={buttonStyle}
           >
             Frost
           </button>
           <button
             onClick={() => {setCategory("mejeri"); setPage(1)}}
-            className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer"
+            className={buttonStyle}
           >
             Mejeri
           </button>
           <button
             onClick={() => {setCategory("kød"); setPage(1)}}
-            className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer"
+            className={buttonStyle}
           >
             Kød
           </button>
           <button
             onClick={() => {setCategory("brød"); setPage(1)}}
-            className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px]  hover:text-[#d4793a] hover:cursor-pointer"
+            className={buttonStyle}
           >
             Brød
           </button>
           <button
             onClick={() => {setCategory("kolonial"); setPage(1)}}
-            className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px]  hover:text-[#d4793a] hover:cursor-pointer"
+            className={buttonStyle}
           >
             Kolonial
           </button>
           <button
             onClick={() => {setCategory("drikkevarer"); setPage(1)}}
-            className="text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px]  hover:text-[#d4793a] hover:cursor-pointer"
+            className={buttonStyle}
           >
             Drikkevarer
           </button>
--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,15 +4,15 @@ export default function Pagination({ page, totalPages, setPage }){
 
     return (
       <div className="m-auto flex flex-row justify-center align-middle gap-2 p-2">
-        <button className={buttonStyle} onClick={() => setPage(1)}>First page</button>
+        <button className={buttonStyle} onClick={() => setPage(1)}>Første side</button>
         <button className={buttonStyle} onClick={() => setPage(prevPage => prevPage - 1)} disabled={page === 1}>
-          Previous
+          Forrige
         </button>
-        <span className="h-[30px] px-4 bg-white font-normal text-black lg:text-[20px] md:text-[20px] sm:text-[15px]">Page {page} of {totalPages}</span>
+        <span className="h-[30px] px-4 bg-white font-normal text-black lg:text-[20px] md:text-[20px] sm:text-[15px]">Side {page} ud af {totalPages}</span>
         <button className={buttonStyle} onClick={() => setPage(prevPage => prevPage + 1)} disabled={page === totalPages}>
-          Next
+          Næste
         </button>
-        <button className={buttonStyle} onClick={() => setPage(totalPages)}>Last page</button>
+        <button className={buttonStyle} onClick={() => setPage(totalPages)}>Sidste side</button>
       </div>
     );
   }
