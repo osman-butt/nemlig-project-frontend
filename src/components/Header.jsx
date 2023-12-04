f2f78acc5f951fcd65ef3a37e5d7e840ebc490d7
osman-butt <butt.osman@gmail.com>
2023-12-04 19:26:16 +0100

    Added auth state

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import MobileNav from "./header/mobile/MobileNav";
 import DesktopNav from "./header/desktop/DesktopNav";
 import NavLogo from "./header/NavLogo";
+import useAuth from "../hooks/useAuth";
 
 export default function Header({ basket }) {
   const [nav, setNav] = useState(true);
+  const { auth } = useAuth();
 
   const handleNav = () => {
     setNav(!nav);
@@ -13,7 +15,7 @@ export default function Header({ basket }) {
   return (
     <div className="flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black uppercase font-medium">
       <NavLogo />
-      <DesktopNav basket={basket} />
+      <DesktopNav basket={basket} auth={auth} />
       <MobileNav handleNav={handleNav} basket={basket} nav={nav} />
     </div>
   );
