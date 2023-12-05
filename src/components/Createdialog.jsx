be2770cc6e0cb0da2923703a445c08098bb70f3a
gustavwiese <gusse12345@gmail.com>
2023-12-05 18:37:50 +0100

    Update Createdialog.jsx

--- src/components/Createdialog.jsx
+++ src/components/Createdialog.jsx
@@ -5,18 +5,8 @@ export default function Createdialog({ closeDialog }) {
   const handleAddProduct = async (event) => {
     event.preventDefault();
 
-    const productName = event.target.elements.productName.value;
-    const price = event.target.elements.price.value;
-    const image = event.target.elements.image.value;
-
-    const formData = {
-      productName,
-      price,
-      image,
-    };
-
     try {
-      const response = await axios.post("http://localhost:3000/api/v1/add-product", formData);
+      const response = await axios.post("/products");
 
       if (response.status === 200) {
         console.log("Produkt tilføjet!");
@@ -30,23 +20,41 @@ export default function Createdialog({ closeDialog }) {
   };
 
   return (
-    <div className="fixed inset-0 pt-10 items-center justify-center z-50 bg-black bg-opacity-50">
+    <div className="fixed overflow-scroll inset-0 pt-10 items-center justify-center z-50 bg-black bg-opacity-50">
       <dialog open className="rounded-md">
         <h1 className="text-center text-2xl font-bold py-3">Tilføj vare</h1>
-        <form className="gap-y-2 px-10 grid">
+        <form onSubmit={handleAddProduct} className="gap-y-2 px-10 grid">
           <FormInput
             label="Produktnavn:"
             type="text"
             placeholder="Skriv navn på produkt her"
             name="productName"
           />
-          <FormInput label="Pris:" type="text" placeholder="Skriv pris på varen her" name="price" />
+          <FormInput
+            label="Produkt understregning:"
+            type="text"
+            placeholder="Skriv produkt understregning her"
+            name="productUnderline"
+          />
+          <FormInput
+            label="Produktbeskrivelse:"
+            type="text"
+            placeholder="Skriv produktbeskrivelse her"
+            name="productDescription"
+          />
           <FormInput
             label="Billede:"
             type="text"
             placeholder="Indsæt link til billede her"
-            name="image"
+            name="productImage"
           />
+          <FormInput
+            label="Pris:"
+            type="text"
+            placeholder="Skriv pris på varen her"
+            name="productPrice"
+          />
+
           <div>
             <button
               className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md mb-4 my-2 py-2 text-black font-medium text-xl"
@@ -55,8 +63,8 @@ export default function Createdialog({ closeDialog }) {
               Luk
             </button>
             <button
-              className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md ml-[7rem] mb-4 my-2 py-2 text-black font-medium text-xl"
-              onClick={handleAddProduct}
+              className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md ml-[11.5rem] mb-4 my-2 py-2 text-black font-medium text-xl"
+              type="submit"
             >
               Tilføj
             </button>
