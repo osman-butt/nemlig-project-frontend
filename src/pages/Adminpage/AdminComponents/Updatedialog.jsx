7cbccca9d5ceddc382818f013a6cb603dcdcb924
gustavwiese <gusse12345@gmail.com>
2023-12-07 10:24:47 +0100

    moved button to the right

--- src/pages/Adminpage/AdminComponents/Updatedialog.jsx
+++ src/pages/Adminpage/AdminComponents/Updatedialog.jsx
@@ -1,11 +1,14 @@
 import FormInput from "../../../components/FormInput";
-import { handleInputChange, handleImageChange, handleIsCampaignChange, handlePriceOrDateChange } from "../AdminUtils/eventHandlers";
+import {
+  handleInputChange,
+  handleImageChange,
+  handleIsCampaignChange,
+  handlePriceOrDateChange,
+} from "../AdminUtils/eventHandlers";
 import axios from "../../../api/axios";
 import { useState } from "react";
 
 export default function Updatedialog({ closeDialog, data, labelData, categoryData }) {
-  
-
   const [productData, setProductData] = useState({
     product_name: data.product_name,
     product_underline: data.product_underline,
@@ -24,7 +27,7 @@ export default function Updatedialog({ closeDialog, data, labelData, categoryDat
     ],
   });
 
-  async function handleUpdateProduct(event){
+  async function handleUpdateProduct(event) {
     event.preventDefault();
     try {
       const response = await axios.put(`/products/${data.product_id}`, productData);
@@ -47,42 +50,105 @@ export default function Updatedialog({ closeDialog, data, labelData, categoryDat
       <dialog open className="">
         <h1 className="text-center text-2xl font-bold py-3">Opdater vare</h1>
         <form onSubmit={handleUpdateProduct} className="gap-y-2 px-10 grid">
-          <FormInput label="Produktnavn:" type="text" placeholder="Skriv navn på produkt her" name="product_name" value={productData.product_name} onChange={handleInputChange}/>
-          <FormInput label="Produkt understregning:" type="text" placeholder="Skriv navn på produkt her" name="product_underline" value={productData.product_underline} onChange={handleInputChange} />
-          <FormInput label="Produktbeskrivelse:" type="text" placeholder="Skriv navn på produkt her" name="product_description" value={productData.product_description} onChange={handleInputChange} />
-          <FormInput label="Billede:" type="text" placeholder="Indsæt link til billede her" value={productData.images[0].image_url} />
-          <label className="font-bold"> Label:
-          <select className="mt-2 w-full px-4 py-3 leading-tight text-black border rounded shadow focus:outline-none focus:shadow-outline">
-            <option value={data.labels[0] && data.labels[0].label_id}>{data.labels[0] && data.labels[0].label_name ? data.labels[0].label_name : "Vælg label"}</option>
-            {labelData.map((label) => (
-              <option key={label.label_id} value={label.label_id}>
-                {label.label_name}
+          <FormInput
+            label="Produktnavn:"
+            type="text"
+            placeholder="Skriv navn på produkt her"
+            name="product_name"
+            value={productData.product_name}
+            onChange={handleInputChange}
+          />
+          <FormInput
+            label="Produkt understregning:"
+            type="text"
+            placeholder="Skriv navn på produkt her"
+            name="product_underline"
+            value={productData.product_underline}
+            onChange={handleInputChange}
+          />
+          <FormInput
+            label="Produktbeskrivelse:"
+            type="text"
+            placeholder="Skriv navn på produkt her"
+            name="product_description"
+            value={productData.product_description}
+            onChange={handleInputChange}
+          />
+          <FormInput
+            label="Billede:"
+            type="text"
+            placeholder="Indsæt link til billede her"
+            value={productData.images[0].image_url}
+          />
+          <label className="font-bold">
+            {" "}
+            Label:
+            <select className="mt-2 w-full px-4 py-3 leading-tight text-black border rounded shadow focus:outline-none focus:shadow-outline">
+              <option value={data.labels[0] && data.labels[0].label_id}>
+                {data.labels[0] && data.labels[0].label_name
+                  ? data.labels[0].label_name
+                  : "Vælg label"}
               </option>
-            ))}
-          </select>
+              {labelData.map((label) => (
+                <option key={label.label_id} value={label.label_id}>
+                  {label.label_name}
+                </option>
+              ))}
+            </select>
           </label>
-          <label className="font-bold"> Kategori:
-          <select className="mt-2 w-full px-4 py-3 leading-tight text-black border rounded shadow focus:outline-none focus:shadow-outline">
-            <option value={data.categories[0] && data.categories[0].category_id}>{data.categories[0] && data.categories[0].category_name ? data.categories[0].category_name : "Vælg kategori"}</option>
-            {categoryData.map((category) => (
-              <option key={category.category_id} value={category.category_id}>
-                {category.category_name}
+          <label className="font-bold">
+            {" "}
+            Kategori:
+            <select className="mt-2 w-full px-4 py-3 leading-tight text-black border rounded shadow focus:outline-none focus:shadow-outline">
+              <option value={data.categories[0] && data.categories[0].category_id}>
+                {data.categories[0] && data.categories[0].category_name
+                  ? data.categories[0].category_name
+                  : "Vælg kategori"}
               </option>
-            ))}
-          </select>
+              {categoryData.map((category) => (
+                <option key={category.category_id} value={category.category_id}>
+                  {category.category_name}
+                </option>
+              ))}
+            </select>
           </label>
-          <FormInput label="Antal på lager:" type="text" placeholder="Skriv antal her" name="inventory_stock" value={productData.inventory_stock} />
+          <FormInput
+            label="Antal på lager:"
+            type="text"
+            placeholder="Skriv antal her"
+            name="inventory_stock"
+            value={productData.inventory_stock}
+          />
           <fieldset className="border-4 border-solid black">
-          {productData.prices.map((price, index) => (
-          <div key={index}>
-            <FormInput label="Pris:" type="text" placeholder="Skriv pris på varen her" value={price.price + " kr."} />
-            <div>
-              <p>Kampagne</p>
-              <input type="checkbox" checked={price.is_campaign || false} onChange={event => handleIsCampaignChange(index)(event.target.checked)}/>
-            </div>
-            <FormInput label="Start dato" type="date" value={price.starting_at ? new Date(price.starting_at).toISOString().split('T')[0] : ""}  />
-            <FormInput label="Slut dato" type="date" value={new Date(price.ending_at).toISOString().split('T')[0]} />
-          </div>
+            {productData.prices.map((price, index) => (
+              <div key={index}>
+                <FormInput
+                  label="Pris:"
+                  type="text"
+                  placeholder="Skriv pris på varen her"
+                  value={price.price + " kr."}
+                />
+                <div>
+                  <p>Kampagne</p>
+                  <input
+                    type="checkbox"
+                    checked={price.is_campaign || false}
+                    onChange={(event) => handleIsCampaignChange(index)(event.target.checked)}
+                  />
+                </div>
+                <FormInput
+                  label="Start dato"
+                  type="date"
+                  value={
+                    price.starting_at ? new Date(price.starting_at).toISOString().split("T")[0] : ""
+                  }
+                />
+                <FormInput
+                  label="Slut dato"
+                  type="date"
+                  value={new Date(price.ending_at).toISOString().split("T")[0]}
+                />
+              </div>
             ))}
           </fieldset>
           <div>
@@ -92,7 +158,7 @@ export default function Updatedialog({ closeDialog, data, labelData, categoryDat
             >
               Luk
             </button>
-            <button className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md ml-[7rem] mb-4 my-2 py-2 text-black font-medium text-xl">
+            <button className="bg-[rgb(212,121,58)] hover:bg-[#ecbc9a] w-[100px] rounded-md ml-[11.5rem] py-2 text-black font-medium text-xl">
               Opdater
             </button>
           </div>
