4129bf06be133f1261a753bd6064b1a88c87da1d
osman-butt <butt.osman@gmail.com>
2023-12-08 01:34:53 +0100

    Removed shipping info

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,45 +1,22 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import useCart from "../hooks/useCart";
 import Listitem from "./Listitem";
-import usePrivateAxios from "../hooks/usePrivateAxios.js";
 import useAuth from "../hooks/useAuth";
 import { useNavigate } from "react-router-dom";
 
 function List() {
   const { cart } = useCart();
-  const [customer, setCustomer] = useState();
   const { auth } = useAuth();
-
   const navigate = useNavigate();
 
-  const privateAxios = usePrivateAxios();
-  useEffect(() => {
-    const fetchCustomer = async () => {
-      const response = await privateAxios.get("/customers");
-      console.log(response.data);
-      setCustomer(response.data);
-    };
-    auth?.accessToken ? fetchCustomer() : setCustomer();
-  }, [auth]);
   return (
     <div className="max-w-[900px] w-full min-h-screen mx-auto text-center flex flex-col font-general">
       <div className="flex flex-row justify-center mt-20 bg-[#e8e3d8] rounded">
         <section className="container w-full p-4 mb-4 rounded md:p-10">
-          <div className="flex flex-row justify-between align-middle mb-2">
-            <h1 className="pb-2 text-4xl text-left md:pb-4 align-middle">
+          <div className="flex flex-row justify-between mb-2 align-middle">
+            <h1 className="pb-2 text-4xl text-left align-middle md:pb-4">
               Din kurv
             </h1>
-            {customer && (
-              <div className="text-left">
-                <h3 className="font-bold">Sendes til:</h3>
-                <p>{customer?.customer_name}</p>
-                <p>{customer?.addresses[0]?.street}</p>
-                <p>
-                  {customer?.addresses[0]?.zip_code}{" "}
-                  {customer?.addresses[0]?.city}
-                </p>
-              </div>
-            )}
           </div>
           <table className="w-[100%] select-none" id="cart">
             <thead className="border-b-2 border-black">
