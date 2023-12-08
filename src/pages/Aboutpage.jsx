ec034627a6287dc57c91590cd445ca35ef0fab69
gustavwiese <gusse12345@gmail.com>
2023-12-08 11:02:29 +0100

    aboutpage done

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,5 +1,6 @@
 import image from "../assets/hero.jpg";
 import Footer from "../components/Footer";
+import About from "../components/About";
 
 export default function Aboutpage() {
   return (
@@ -7,6 +8,7 @@ export default function Aboutpage() {
       className="min-h-screen bg-fixed bg-center bg-cover"
       style={{ backgroundImage: `url(${image})` }}
     >
+      <About />
       <Footer />
     </div>
   );
