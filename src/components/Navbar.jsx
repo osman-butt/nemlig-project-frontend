b51291df0574fbc6ee279832b18bf9df7f5a8716
Benjamin-Harris1 <beha1503@gmail.com>
2023-12-10 22:30:37 +0100

    if setPage is present, set page (its not on favoritesPage)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,83 @@
 export default function Navbar({ setCategory, setPage }) {
-
-  const buttonStyle = "text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer"
+  const buttonStyle = "text-base px-4 md:text-lg lg:py-4 lg:text-xl font-normal text-white text-[18px] hover:text-[#d4793a] hover:cursor-pointer";
 
   return (
     <nav>
       <div className="bg-[#58644c] w-full font-general">
         <div className="max-w-[1240px] flex flex-wrap justify-center md:justify-start m-auto z-50 py-2 align-middle">
           <button
-          onClick={() => setCategory("")}
-          className={buttonStyle}
+            onClick={() => {
+              setCategory("");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
+            className={buttonStyle}
           >
             Alle
           </button>
           <button
-            onClick={() => {setCategory("frost"); setPage(1)}}
+            onClick={() => {
+              setCategory("frost");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
             className={buttonStyle}
           >
             Frost
           </button>
           <button
-            onClick={() => {setCategory("mejeri"); setPage(1)}}
+            onClick={() => {
+              setCategory("mejeri");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
             className={buttonStyle}
           >
             Mejeri
           </button>
           <button
-            onClick={() => {setCategory("kød"); setPage(1)}}
+            onClick={() => {
+              setCategory("kød");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
             className={buttonStyle}
           >
             Kød
           </button>
           <button
-            onClick={() => {setCategory("brød"); setPage(1)}}
+            onClick={() => {
+              setCategory("brød");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
             className={buttonStyle}
           >
             Brød
           </button>
           <button
-            onClick={() => {setCategory("kolonial"); setPage(1)}}
+            onClick={() => {
+              setCategory("kolonial");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
             className={buttonStyle}
           >
             Kolonial
           </button>
           <button
-            onClick={() => {setCategory("drikkevarer"); setPage(1)}}
+            onClick={() => {
+              setCategory("drikkevarer");
+              if (setPage) {
+                setPage(1);
+              }
+            }}
             className={buttonStyle}
           >
             Drikkevarer
