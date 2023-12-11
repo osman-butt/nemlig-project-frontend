f86ece785bc088c598f455a2b963d2f088107a70
osman-butt <butt.osman@gmail.com>
2023-12-11 12:22:16 +0100

    Non empty cart redirect to /cart

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,8 +1,19 @@
 import image from "../../assets/hero.jpg";
 import CheckoutList from "./CheckoutList";
 import Footer from "../../components/Footer";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import useCart from "../../hooks/useCart";
 
 function CheckoutPage() {
+  const navigate = useNavigate();
+
+  const { cart } = useCart();
+
+  useEffect(() => {
+    cart?.length === 0 && navigate("/cart");
+  }, []);
+
   return (
     <div
       className="bg-fixed bg-center bg-cover"
