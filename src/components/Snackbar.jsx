3c9d06662ddc4248845d06878eb7f3842e312a7a
osman-butt <butt.osman@gmail.com>
2023-12-12 23:49:28 +0100

    Updated snackbar height

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -1,6 +1,6 @@
 export default function Snackbar() {
   return (
-    <section className="absolute z-50 w-full h-[30px] bg-[#58644c] text-white top-0 left-0 mx-auto rounded-t-xl flex flex-row justify-center align-middle">
+    <section className="absolute z-50 w-full py-3 bg-[#58644c] text-white top-0 left-0 mx-auto rounded-t-xl flex flex-row justify-center align-middle">
       <p className="mx-auto">Tilføjet til kurv</p>
     </section>
   );
