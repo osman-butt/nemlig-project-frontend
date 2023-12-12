3c4d534d8ecea5a558e43305a5bfe18e993666be
osman-butt <butt.osman@gmail.com>
2023-12-12 22:45:44 +0100

    Added styling to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -14,15 +14,6 @@ export default function Card({
 }) {
   const { incrementCartItem } = useCart();
   const [showSnackbar, setShowSnackbar] = useState(false);
-  // const [showBar, setShowBar] = useState(false);
-
-  // const showSnackybar = () => {
-  //   setShowSnackbar(true);
-
-  //   setTimeout(() => {
-  //     setShowSnackbar(false);
-  //   }, 3000); // Skjul snackbar efter 3 sekunder (justér efter behov)
-  // };
 
   function showSnackybar() {
     setShowSnackbar(true);
@@ -48,49 +39,65 @@ export default function Card({
       : null;
 
   return (
-    <article className="bg-white rounded flex flex-col items-center gap-2 p-4 w-[200px] h-[450px] justify-self-center">
-      {(alwaysShowStar || (auth && auth.user_email)) && (
-        <img
-          className="translate-x-[74px]"
-          alt="Star"
-          src={data.favorite_id ? filledstar : star}
-          onClick={() =>
-            data.favorite_id ? removeFromFavorites(data) : addToFavorites(data)
-          }
-        />
-      )}
-      <img
-        className="object-contain w-[150px] h-[150px]"
-        src={data.images && data.images[0] ? data.images[0].image_url : ""}
-        alt="productImage"
-      />
-      <p className="mt-auto font-medium text-center">{data.product_name}</p>
-      <p className="font-light text-[14px] mt-auto">{data.product_underline}</p>
-      {lowestPrice && lowestPrice.is_pricematch && lowestPrice.is_pricematch ? (
-        <>
-          <h3 className="font-bold text-red-500">PRISMATCH</h3>
-          <p className="text-[18px] mt-auto line-through">
-            {highestPrice.price.toFixed(2)} kr.
+    <article className="bg-white rounded-xl w-[200px] h-[350px] relative mx-auto pt-4">
+      {showSnackbar && <Snackbar />}
+      <div className="flex flex-col items-center w-full h-full gap-2 justify-self-center">
+        {(alwaysShowStar || (auth && auth.user_email)) && (
+          <img
+            className="translate-x-[74px]"
+            alt="Star"
+            src={data.favorite_id ? filledstar : star}
+            onClick={() =>
+              data.favorite_id
+                ? removeFromFavorites(data)
+                : addToFavorites(data)
+            }
+          />
+        )}
+        <div className="relative">
+          <img
+            className="pt-2 object-contain w-[130px] h-[130px]"
+            src={data.images && data.images[0] ? data.images[0].image_url : ""}
+            alt="productImage"
+          />
+          {/* <div className="absolute top-10 left-0 z-10 flex flex-row justify-center w-full h-10 text-black align-middle bg-[#d4793a] bg-opacity-70 rounded-2xl">
+            <p className="self-center">Prismatch</p>
+          </div> */}
+        </div>
+        <div>
+          <p className="mt-auto font-medium text-center">{data.product_name}</p>
+          <p className="font-light text-[14px] mt-auto">
+            {data.product_underline}
           </p>
-          <p className="font-bold text-[18px] mt-auto">
-            {lowestPrice.price.toFixed(2)} kr.
+        </div>
+        {lowestPrice &&
+        lowestPrice.is_pricematch &&
+        lowestPrice.is_pricematch ? (
+          <>
+            <h3 className="font-bold text-red-500">PRISMATCH</h3>
+            <p className="text-[18px] my-auto line-through text-red-700">
+              {highestPrice.price.toFixed(2)} kr.
+            </p>
+            <p className="font-bold text-[18px] my-auto align-middle">
+              {lowestPrice.price.toFixed(2)} kr.
+            </p>
+          </>
+        ) : (
+          <p className="font-bold text-[18px] my-auto align-middle">
+            {lowestPrice && lowestPrice.price.toFixed(2)} kr.
           </p>
-        </>
-      ) : (
-        <p className="font-bold text-[18px] mt-auto">
-          {lowestPrice && lowestPrice.price.toFixed(2)} kr.
-        </p>
-      )}
-      <button
-        onClick={() => {
-          incrementCartItem(data);
-          showSnackybar();
-        }}
-        className="bg-[#d4793a] p-2 rounded"
-      >
-        Læg i kurv
-      </button>
-      {showSnackbar && <Snackbar showSnackbar={showSnackbar} />}
+        )}
+        <button
+          onClick={() => {
+            incrementCartItem(data);
+            showSnackybar();
+          }}
+          className="bg-[#d4793a] rounded-b-xl w-full py-3 text-white"
+        >
+          Læg i kurv
+        </button>
+        {showSnackbar && <Snackbar showSnackbar={showSnackbar} />}
+      </div>
     </article>
   );
 }
