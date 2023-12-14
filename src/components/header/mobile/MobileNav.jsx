1746ffde76e08634e268961d482089ee83412681
osman-butt <butt.osman@gmail.com>
2023-12-14 04:43:40 +0100

    Fixed mob nav bar

--- src/components/header/mobile/MobileNav.jsx
+++ src/components/header/mobile/MobileNav.jsx
@@ -2,8 +2,22 @@ import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import MobileNavItem from "./MobileNavItem";
 import MobileNavBasket from "./MobileNavBasket";
 import MobileNavLogo from "./MobileNavLogo";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAuth from "../../../hooks/useAuth";
+import useLogout from "../../../hooks/useLogout";
 
 function MobileNav({ handleNav, nav }) {
+  const location = useLocation();
+  const logout = useLogout();
+  const { auth } = useAuth();
+
+  const navigate = useNavigate();
+
+  const signOut = async () => {
+    await logout();
+    handleNav();
+    navigate("/login");
+  };
   return (
     <div className="relative z-50">
       <div onClick={handleNav} className="block md:hidden hover:cursor-pointer">
@@ -29,19 +43,47 @@ function MobileNav({ handleNav, nav }) {
             </>
           ) : (
             <>
-              <MobileNavItem handleNav={handleNav} path="/shop">
-                Dagligvarer
-              </MobileNavItem>
-              <MobileNavItem handleNav={handleNav} path="/favorites">
-                Favoritter
-              </MobileNavItem>
-              <MobileNavItem handleNav={handleNav} path="/login">
-                Log ind
-              </MobileNavItem>
-              <MobileNavItem handleNav={handleNav} path="/createaccount">
-                Opret bruger
-              </MobileNavItem>
-              <MobileNavBasket handleNav={handleNav} />
+              {!auth?.user_roles?.includes("admin") && (
+                <MobileNavItem handleNav={handleNav} path="/shop">
+                  Dagligvarer
+                </MobileNavItem>
+              )}
+
+              {auth?.user_roles?.includes("admin") && (
+                <>
+                  <MobileNavItem handleNav={handleNav} path="/admin">
+                    Admin
+                  </MobileNavItem>
+                </>
+              )}
+              {auth?.user_roles?.includes("customer") && (
+                <MobileNavItem handleNav={handleNav} path="/favorites">
+                  Favoritter
+                </MobileNavItem>
+              )}
+              {auth?.accessToken && (
+                <button
+                  className="p-4 border-b border-[#d4793a] font-bold hover:text-[#d4793a] hover:cursor-pointer w-full text-left"
+                  onClick={signOut}
+                >
+                  LOG UD
+                </button>
+              )}
+              {!auth?.accessToken && (
+                <>
+                  <MobileNavItem handleNav={handleNav} path="/login">
+                    Log ind
+                  </MobileNavItem>
+                  <MobileNavItem handleNav={handleNav} path="/createaccount">
+                    Opret bruger
+                  </MobileNavItem>
+                </>
+              )}
+              {!auth?.user_roles?.includes("admin") && (
+                <div className="h-10 align-top">
+                  <MobileNavBasket handleNav={handleNav} />
+                </div>
+              )}
             </>
           )}
         </ul>
